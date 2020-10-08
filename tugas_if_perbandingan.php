8767ccf21d553e7a3fc719f786d07e23f27f0fdc
TRI AFRILIYANTI <71365606+triafri@users.noreply.github.com>
2020-10-08 12:04:59 +0700

    Add files via upload

---
+++ tugas_if_perbandingan.php
@@ -0,0 +1,15 @@
+<?php
+
+	$a=5;
+	$b=5;
+
+	if ($a < $b){
+		echo "$a lebih kecil dari $b";
+	} else if ($a > $b) {
+		echo ("$a lebih besar dari $b");
+	}
+	else{
+		echo ("$a sama dengan $b");
+	}
+
+?>
